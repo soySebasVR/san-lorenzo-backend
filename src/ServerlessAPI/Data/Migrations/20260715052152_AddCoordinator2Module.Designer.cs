12a65768e892e852a806274f8d7bca94359ecdbd
Andre-Lauda <aj.lauda98@gmail.com>
2026-07-15 00:35:24 -0500

    feat(coord2): agrega migracion de base de datos

--- src/ServerlessAPI/Data/Migrations/20260715052152_AddCoordinator2Module.Designer.cs
+++ src/ServerlessAPI/Data/Migrations/20260715052152_AddCoordinator2Module.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServerlessAPI.Data;
 
@@ -11,9 +12,11 @@ using ServerlessAPI.Data;
 namespace ServerlessAPI.Data.Migrations
 {
     [DbContext(typeof(SanLorenzoDbContext))]
-    partial class SanLorenzoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260715052152_AddCoordinator2Module")]
+    partial class AddCoordinator2Module
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
